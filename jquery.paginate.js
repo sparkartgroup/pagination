643c8c01721a57f057affe4bf8b301d6178fd96d
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-06-06 18:31:35 -0300

    Initial skeleton of Resource, *Pagination, and $.fn.paginate

---
+++ jquery.paginate.js
@@ -0,0 +1,104 @@
+/*
+ * Storyteller Pagination Plug-in 0.0.1
+ * @requires jQuery
+ *
+ * Present aggregated content as a single blended and paginated stream.
+ * 
+ * This plugin is available under the MIT License (Expat).
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright © 2013 Story Arc Corporation · storytellerhq.com
+ */
+(function( $, undefined ) {
+
+
+	/**
+	 * Resource
+	 *
+	 * A simple encapsulation.
+	 */
+
+	var Resource = function( url, data ) {};
+
+	Resource.prototype = {};
+
+
+	/**
+	 * Pagination
+	 *
+	 * Extends resource with .next().
+	 */
+
+	var Pagination = function( resource, attributes ) {};
+
+	/**
+	 * CursoringPagination
+	 */
+
+	var CursoringPagination = function( resource, attributes ) {
+		// Pagination super
+		Pagination.apply( this, arguments );
+	};
+
+	CursoringPagination.prototype = {};
+
+	/**
+	 * IndexOffsetPagination
+	 */
+
+	var IndexOffsetPagination = function( resource, attributes ) {
+		// Pagination super
+		Pagination.apply( this, arguments );
+	};
+
+	IndexOffsetPagination.prototype = {};
+
+	/**
+	 * PagePagination
+	 */
+
+	var PagePagination = function( resource, attributes ) {
+		// Pagination super
+		Pagination.apply( this, arguments );
+	};
+
+	PagePagination.prototype = {};
+
+	/**
+	 * TokenPagination
+	 */
+
+	var TokenPagination = function( resource, attributes ) {
+		// Pagination super
+		Pagination.apply( this, arguments );
+	};
+
+	TokenPagination.prototype = {};
+
+
+	/**
+	 * PaginatePlugin
+	 */
+
+	var paginations = {
+		"cursoring": CursoringPagination,
+		"index-offset": IndexOffsetPagination,
+		"page": PagePagination,
+		"token": TokenPagination
+	};
+
+	var PaginatePlugin = function() {};
+
+
+	/**
+	 * $.fn.paginate
+	 */
+
+	$.fn.paginate = function( attributes ) {
+		this.each(function() {
+			PaginatePlugin( this, attributes );
+		});
+		return this;
+	};
+
+})( jQuery );
