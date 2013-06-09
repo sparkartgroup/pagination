1ce4538b98f5bd9054ebd7bc701de5b64d2dfeb9
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-06-09 16:29:55 -0300

    Add success and error events tests

--- test/spec/jquery.paginate.js
+++ test/spec/jquery.paginate.js
@@ -4,6 +4,20 @@
 
 	describe( "$.fn.paginate()", function () {
 
+		var DummyResource = function( attributes ) {
+			attributes = attributes || { flavor: "success" };
+			this.get = DummyResource.prototype.flavors[ attributes.flavor ];
+		};
+		DummyResource.prototype = {};
+		DummyResource.prototype.flavors = {
+			success: function() {
+				return $.Deferred().resolve({ data: [] });
+			},
+			failure: function() {
+				return $.Deferred().reject();
+			}
+		};
+
 		it( "should be defined", function() {
 			expect( $.fn.paginate ).to.be.a( "function" );
 		});
@@ -17,6 +31,26 @@
 					}]);
 				}).to.throw( Error, /unexpected attributes.resource/ );
 			});
+		});
+
+		it( "should emit \"success\" event on fetch success", function( done ) {
+			this.timeout( 1000 );
+			$( "<div>" ).paginate([{
+				resource: new DummyResource(),
+				pagination: { type: "index-offset" }
+			}]).on( "success", function() {
+				done();
+			}).trigger( "next" );
+		});
+
+		it( "should emit \"error\" event on fetch failure", function( done ) {
+			this.timeout( 1000 );
+			$( "<div>" ).paginate([{
+				resource: new DummyResource({ flavor: "failure" }),
+				pagination: { type: "index-offset" }
+			}]).on( "error", function() {
+				done();
+			}).trigger( "next" );
 		});
 
 	});
