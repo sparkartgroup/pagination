180a760d4fd59c4d21b5ea0229e3850c748b81b2
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-06-10 12:05:50 -0300

    Add Index/Offset unit tests

--- test/spec/jquery.paginate.js
+++ test/spec/jquery.paginate.js
@@ -2,21 +2,26 @@
 "use strict";
 (function () {
 
-	describe( "$.fn.paginate()", function () {
+	var DummyResource = function( attributes ) {
+		attributes = attributes || { flavor: "success" };
+		this.get = DummyResource.prototype.flavors[ attributes.flavor ];
+	};
+	DummyResource.prototype = {};
+	DummyResource.prototype.flavors = {
+		success: function() {
+			return $.Deferred().resolve({ data: [] });
+		},
+		failure: function() {
+			return $.Deferred().reject();
+		}
+	};
 
-		var DummyResource = function( attributes ) {
-			attributes = attributes || { flavor: "success" };
-			this.get = DummyResource.prototype.flavors[ attributes.flavor ];
-		};
-		DummyResource.prototype = {};
-		DummyResource.prototype.flavors = {
-			success: function() {
-				return $.Deferred().resolve({ data: [] });
-			},
-			failure: function() {
-				return $.Deferred().reject();
-			}
-		};
+	describe( "$.fn.paginate()", function () {
+		var TIMEOUT = 1000;
+
+		/**
+		 * Basic unit tests
+		 */
 
 		it( "should be defined", function() {
 			expect( $.fn.paginate ).to.be.a( "function" );
@@ -34,7 +39,7 @@
 		});
 
 		it( "should emit \"success\" event on fetch success", function( done ) {
-			this.timeout( 1000 );
+			this.timeout( TIMEOUT );
 			$( "<div>" ).paginate([{
 				resource: new DummyResource(),
 				pagination: { type: "index-offset" }
@@ -44,13 +49,78 @@
 		});
 
 		it( "should emit \"error\" event on fetch failure", function( done ) {
-			this.timeout( 1000 );
+			this.timeout( TIMEOUT );
 			$( "<div>" ).paginate([{
 				resource: new DummyResource({ flavor: "failure" }),
 				pagination: { type: "index-offset" }
 			}]).on( "error", function() {
 				done();
 			}).trigger( "next" );
+		});
+
+		/**
+		 * Index/Offset unit tests
+		 */
+
+		describe( "Index/Offset pagination", function () {
+			var LIMIT = 15;
+			var A_LESS_THAN_LIMIT = 3;
+			var resource = new DummyResource();
+			var element = $( "<div>" ).paginate([{
+				resource: resource,
+				pagination: {
+					type: "index-offset",
+					attributes: { limit: LIMIT }
+				}
+			}]);
+
+			it( "should set `limit=" + LIMIT + "` param", function( done ) {
+				this.timeout( TIMEOUT );
+				resource.get = function( params ) {
+					expect( params.limit ).to.be.a( "number" );
+					expect( params.limit ).to.equal( LIMIT );
+					return $.Deferred().resolve({ data: [] });
+				}
+				element.one( "success", function() {
+					done();
+				}).trigger( "next" );
+			});
+
+			it( "should set `offset=0` param on 1st page", function( done ) {
+				this.timeout( TIMEOUT );
+				resource.get = function( params ) {
+					expect( params.offset ).to.be.a( "number" );
+					expect( params.offset ).to.equal( 0 );
+					return $.Deferred().resolve({ data: new Array( LIMIT ) });
+				}
+				element.one( "success", function() {
+					done();
+				}).trigger( "next" );
+			});
+
+			it( "should set `offset=" + LIMIT + "` param on 2nd page after fetching " + LIMIT + " items", function( done ) {
+				this.timeout( TIMEOUT );
+				resource.get = function( params ) {
+					expect( params.offset ).to.be.a( "number" );
+					expect( params.offset ).to.equal( LIMIT );
+					return $.Deferred().resolve({ data: new Array( A_LESS_THAN_LIMIT ) });
+				}
+				element.one( "success", function() {
+					done();
+				}).trigger( "next" );
+			});
+
+			it( "should set `offset=" + LIMIT + A_LESS_THAN_LIMIT + "` param on 3nd page after fetching " + A_LESS_THAN_LIMIT + " more items", function( done ) {
+				this.timeout( TIMEOUT );
+				resource.get = function( params ) {
+					expect( params.offset ).to.be.a( "number" );
+					expect( params.offset ).to.equal( LIMIT + A_LESS_THAN_LIMIT );
+					return $.Deferred().resolve({ data: [] });
+				}
+				element.one( "success", function() {
+					done();
+				}).trigger( "next" );
+			});
 		});
 
 	});
